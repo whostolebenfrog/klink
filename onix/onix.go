04867813008f3c5fa60554ac1860063bce552f50
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-10-01 15:25:27 +0100

    Update onix to use positional arguments

--- onix/onix.go
+++ onix/onix.go
@@ -15,11 +15,11 @@ func onixUrl(end string) string {
 }
 
 func CreateService(args common.Command) {
-    if args.Application == "" {
-        console.Fail("Must supply an application name")
+    if args.SecondPos == "" {
+        console.Fail("Must supply an application name as second positional argument")
     }
 
-    createBody := Service{args.Application}
+    createBody := Service{args.SecondPos}
 
     response, err := common.PostJson(onixUrl("/applications"), createBody)
 
