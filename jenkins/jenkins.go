36f4b6340faa66164306d864c7976407348c7d82
Ben Griffiths <benjamin.griffiths@nokia.com>
2014-02-12 14:12:07 +0000

    Get the build path

--- jenkins/jenkins.go
+++ jenkins/jenkins.go
@@ -1,13 +1,16 @@
 package jenkins
 
 import (
+	"fmt"
 	common "nokia.com/klink/common"
-    console "nokia.com/klink/console"
+	console "nokia.com/klink/console"
+	onix "nokia.com/klink/onix"
 )
 
 func Build(args common.Command) {
-    if args.SecondPos == "" {
-        console.Fail("Yeah, you're gonna have to tell me what to build...")
-    }
-
+	app := args.SecondPos
+	if app == "" {
+		console.Fail("Yeah, you're gonna have to tell me what to build...")
+	}
+	fmt.Println(onix.GetProperty(app, "releasePath"))
 }
